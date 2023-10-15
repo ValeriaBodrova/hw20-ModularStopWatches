b0297287dfbad1b4f9c6e2145d54b5c694a17640
ValeriaBodrova <110822668+ValeriaBodrova@users.noreply.github.com>
2023-10-15 17:44:23 +0200

    fixed TypeError

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-import Stopwatch from 'stopwatch.js';
-import StopwatchUI from 'stopwatch-ui.js';
+import StopWatch from ' ./stopwatch.js ';
+import StopwatchUI from ' ./stopwatch-ui.js ';
 
 const initialTime = {hours: 0, minutes: 0, seconds: 0};
 const ui = new StopwatchUI(initialTime);
 const onTick = (time) => ui.setTime(time);
-const stopwatch = new Stopwatch(onTick);
+const stopwatch = new StopWatch(onTick);
 
 ui.addStartBtnListener(e => stopwatch.start());
 ui.addPauseBtnListener(e => stopwatch.pause());
