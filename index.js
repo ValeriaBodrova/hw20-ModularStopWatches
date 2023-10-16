82bd2861bc4b8fde8a4d85e1afad1a92dfa1f45e
ValeriaBodrova <110822668+ValeriaBodrova@users.noreply.github.com>
2023-10-16 08:55:10 +0200

    fixing 404 error

--- index.js
+++ index.js
@@ -1,15 +1,7 @@
-import StopWatch from '../stopwatch.js ';
-import StopwatchUI from '../stopwatch-ui.js ';
+import StopWatch from '../stopwatch.js';
+import StopwatchUI from '../stopwatch-ui.js';
 
-
-function initialTime(time) {
-    const hours = String(time.hours).padStart(2, '0');
-    const minutes = String(time.minutes).padStart(2, '0');
-    const seconds = String(time.seconds).padStart(2, '0');
-    
-    display.textContent = `${hours}:${minutes}:${seconds}`;
-}
-
+const initialTime = {hours: 0, minutes: 0, seconds: 0};
 const ui = new StopwatchUI(initialTime);
 const onTick = (time) => ui.setTime(time);
 const stopwatch = new StopWatch(onTick);
