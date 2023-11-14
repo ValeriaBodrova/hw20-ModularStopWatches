c776832c9207ef3fb78fe75f5e31f2a0f1cb2ac0
ValeriaBodrova <110822668+ValeriaBodrova@users.noreply.github.com>
2023-11-14 09:43:29 +0100

    Update stopwatch-ui.js

--- modules/stopwatch-ui.js
+++ modules/stopwatch-ui.js
@@ -5,7 +5,6 @@ export default class StopwatchUI {
         this.seconds = initialTime.seconds;
 
         this.timer = document.createElement('div');
-        this.timer.classList.add('main');
         this.startButton = document.createElement('button');
         this.pauseButton = document.createElement('button');
         this.resetButton = document.createElement('button');
