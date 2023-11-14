f022235e07534e418d3fcec523d100218a809e6b
ValeriaBodrova <110822668+ValeriaBodrova@users.noreply.github.com>
2023-11-14 09:40:18 +0100

    add class to div

--- modules/stopwatch-ui.js
+++ modules/stopwatch-ui.js
@@ -5,6 +5,7 @@ export default class StopwatchUI {
         this.seconds = initialTime.seconds;
 
         this.timer = document.createElement('div');
+        timer.classList.add('main');
         this.startButton = document.createElement('button');
         this.pauseButton = document.createElement('button');
         this.resetButton = document.createElement('button');
